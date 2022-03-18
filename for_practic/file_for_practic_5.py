e2e0dc44bd23750310a725264c9345889ea9618e
Student <zhulin@1060.ru>
2022-03-18 15:16:44 +0300

    попустил бота

---
+++ for_practic/file_for_practic_5.py
@@ -0,0 +1,15 @@
+def generate_numbers(n:int, m: int, prefix = None) -> None:
+    """
+    выводит в консоь перестоновки
+    """
+    if m == 0:
+        print(prefix, end= ' ')
+    else:
+        prefix = prefix or []
+        for digitals in range(n):
+            prefix.append(digitals)
+            generate_numbers(n, m - 1, prefix)
+            prefix.pop()
+
+
+generate_numbers(int(input()), int(input()))
